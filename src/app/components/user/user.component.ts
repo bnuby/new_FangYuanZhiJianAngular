920ad560cb65cc49267ea9e4b1024e7c4d32110e
Jacky Tan <jackytan@Jackys-MBP.local>
2018-09-09 20:51:16 +0800

    fix home image box size

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -66,6 +66,7 @@ export class UserComponent implements OnInit {
           if(msg.profile_picture == "") {
             $('#profilePicture').attr('src', "../../../assets/img/logo.png")
           } else {
+            //this got some problem, author != user, need to add author option
             $('#profilePicture').attr('src', `${host}/users/get_pic/${id}`)
           }
         } else {
